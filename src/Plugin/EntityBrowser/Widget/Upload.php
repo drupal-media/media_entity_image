16428b1b5a34d7bba427613ae55f8a50a9afb51e
defr <defr@59710.no-reply.drupal.org>
2016-04-19 10:11:07 +0200

    Issue #2512720 by DeFr, slashrsm: Create entity browser upload plugin for media entity images.

--- src/Plugin/EntityBrowser/Widget/Upload.php
+++ src/Plugin/EntityBrowser/Widget/Upload.php
@@ -7,6 +7,7 @@
 namespace Drupal\media_entity_image\Plugin\EntityBrowser\Widget;
 
 use Drupal\Core\Form\FormStateInterface;
+use Drupal\Core\Url;
 use Drupal\entity_browser\Plugin\EntityBrowser\Widget\Upload as FileUpload;
 
 /**
@@ -36,11 +37,11 @@ class Upload extends FileUpload {
   public function getForm(array &$original_form, FormStateInterface $form_state, array $aditional_widget_parameters) {
     /** @var \Drupal\media_entity\MediaBundleInterface $bundle */
     if (!$this->configuration['media bundle'] || !($bundle = $this->entityManager->getStorage('media_bundle')->load($this->configuration['media bundle']))) {
-      return ['#markup' => t('The media bundle is not configured correctly.')];
+      return ['#markup' => $this->t('The media bundle is not configured correctly.')];
     }
 
     if ($bundle->getType()->getPluginId() != 'image') {
-      return ['#markup' => t('The configured bundle is not using image plugin.')];
+      return ['#markup' => $this->t('The configured bundle is not using image plugin.')];
     }
 
     $form = parent::getForm($original_form, $form_state, $aditional_widget_parameters);
@@ -72,6 +73,55 @@ class Upload extends FileUpload {
 
     $this->selectEntities($images, $form_state);
     $this->clearFormValues($element, $form_state);
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function buildConfigurationForm(array $form, FormStateInterface $form_state) {
+    $form = parent::buildConfigurationForm($form, $form_state);
+    $form['extensions'] = [
+      '#type' => 'textfield',
+      '#title' => $this->t('Allowed extensions'),
+      '#default_value' => $this->configuration['extensions'],
+      '#required' => TRUE,
+    ];
+
+    $bundle_options = [];
+
+    $bundles = $this
+      ->entityManager
+      ->getStorage('media_bundle')
+      ->loadByProperties(['type' => 'image']);
+
+    foreach ($bundles as $bundle) {
+      $bundle_options[$bundle->id()] = $bundle->label();
+    }
+
+    switch (count($bundle_options)) {
+      case 0:
+        $url = Url::fromRoute('media.bundle_add')->toString();
+        $form['media bundle'] = [
+          '#markup' => $this->t("You don't have media bundle of the Image type. You should <a href='!link'>create one</a>", ['!link' => $url]),
+        ];
+        break;
+
+      case 1:
+        $form['media bundle'] = array(
+          '#value' => key($bundle_options),
+        );
+        break;
+
+      default:
+        $form['media bundle'] = array(
+          '#type' => 'select',
+          '#title' => $this->t('Media bundle'),
+          '#default_value' => $this->configuration['media bundle'],
+          '#options' => $bundle_options,
+        );
+    }
+
+    return $form;
   }
 
 }
